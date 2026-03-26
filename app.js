453098835b3635fb182b41365adbe97b22240b7b
Ranvir172005@gmail.com <ranvir172005@gmail.com>
2026-03-26 11:25:40 -0600

    update

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ express = require('express');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 8000;
+const ejs = require('ejs');
 const userRoutes = require('./userRoutes');
+
+//view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
 //middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 app.use(usrRoutes);
 
 //route handler
